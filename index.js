8dc52b8831aababd12acfb49299d3792955bd983
Noelle <noelle.maingi@student.moringaschool.com>
2023-07-06 21:02:29 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const len = word.length; // get the length of the string
+  for (let i = 0; i < len / 2; i++) {
+    // iterate over the string from both ends, comparing the characters
+    if (word[i] !== word[len - 1 - i]) {
+      // if any characters don't match, the string is not a palindrome
+      return false;
+    }
+  }
+  return true;
 }
+
 
 /* 
   Add your pseudocode here
 */
+//1. Declare a function named isPalindrome that takes a parameter 'word'
 
+//2. Get the length of the 'word' and assign it to a variable 'length'.
+
+//3. Start a loop from 'i = 0' to i < length/2'.
+
+//4. Inside the loop, compare word[i] with word[length - 1 - i].
+
+//5. If the words are not equal, return false as the word is not a palindrome.
+
+//6. If the loop completes without finding any mismatch return true as the word is a palindrome.
 /*
   Add written explanation of your solution here
 */
